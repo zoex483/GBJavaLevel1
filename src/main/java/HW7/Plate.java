6b7a9a999174dc3ada70605425e0134e174a06db
zoex483 <91542618+zoex483@users.noreply.github.com>
2021-11-06 16:54:44 +0300

    HW6 v1 final

---
+++ src/main/java/HW7/Plate.java
@@ -0,0 +1,17 @@
+package HW7;
+
+public class Plate {
+    public int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void increaseFood(int x) {
+        food += x;
+    }
+    public void info() {
+        System.out.println("Еды всего: " + food);
+    }
+}
